2fbf5eb4c36ac6ed2c4eaac37dc206f4730b4544
Ivan França <contato.ivanfrancajr@gmail.com>
2023-05-07 11:44:25 -0300

    feat: alterado etrutura de pastas + setup inicial

---
+++ 3 - Conceitos Avançados de React com TypeScript/src/components/Card.tsx
@@ -0,0 +1,32 @@
+import { Text, Input, Flex, Button } from "@chakra-ui/react"
+import { useState } from "react"
+interface IProp {
+  onClick: () => void
+}
+
+export const Card = ({ onClick }: IProp) => {
+  
+  const [email,setEmail] = useState('')
+  const [password,setPassword] = useState('')
+  return (
+    <Flex direction={"column"}
+      alignItems="center"
+      minHeight={'469px'}
+      minWidth={'320px'}
+      padding={90}
+      borderRadius={10}
+      fontSize={24}
+      backgroundColor={'#fff'}
+       >
+      <Flex
+      direction={"column"} alignItems={'center'} >
+        <Text marginBottom={45} fontSize={20} color={"black"} >
+          Faça seu login
+        </Text>
+        <Input placeholder={'Email'} marginBottom={3} type='text' color={"black"} value={email} onChange={(e) => setEmail(e.target.value)}/>
+        <Input placeholder={'Senha'} marginBottom={3} type='password' color={"black"} value={password } onChange={(e) => setPassword(e.target.value)}/>
+        <Button color={"black"} backgroundColor={'#a1b567'} onClick={onClick}> Acessar</Button>
+     </Flex>
+    </Flex>
+  )
+}
