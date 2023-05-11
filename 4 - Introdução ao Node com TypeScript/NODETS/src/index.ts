99ea876da350545070aad5d39bf61e952324099b
Ivan França <contato.ivanfrancajr@gmail.com>
2023-05-11 17:16:49 -0300

    backup

--- 4 - Introdução ao Node com TypeScript/NODETS/src/index.ts
+++ 4 - Introdução ao Node com TypeScript/NODETS/src/index.ts
@@ -1,3 +1,15 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+
 const server = express();
-server.listen('3001', () => console.log('server up 🔥'));
+server.use(express.json());
+
+server.listen('3001', () => console.log('server up! 🔥'));
+
+server.get('/', (req: Request, res: Response) => {
+  res.status(200).json({ message: 'Olá amigos do mundo!' });
+});
+server.post('/user', (req: Request, res: Response) => {
+  const body = req.body;
+  console.log(body);
+  return res.status(201).json({ message: 'usuário criado' });
+});
